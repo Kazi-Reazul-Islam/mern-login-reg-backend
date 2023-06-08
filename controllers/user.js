b0697c422fb51f853bd80f3138edeb8ab9bcd48b
Kazi Reazul Islam <kazireazulislam1999@gmail.com>
2023-06-08 12:24:23 +0600

    registerUser function added to controllers>user.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,26 @@
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 exports.registerUser = async (req, res) => {
+  const { first_name, last_Name, email, password } = req.body;
+
+  const exist = await User.findOne({ email: email.toLowerCase() }).exec();
+
+  if (exist) {
+    return res.status(401).json({
+      message: "Email Already Exist",
+    });
+  }
+
+  const bcryptedPassword = await bcrypt.hash("password", 12);
+
+  await new User({
+    first_name,
+    last_Name,
+    email,
+    password: bcryptedPassword,
+  }).save();
+
   res.send({
     message: "Register Success",
   });
